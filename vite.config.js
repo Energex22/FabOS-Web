c789b0950efa1265086d759a83b80c3b09a1a2ed
Energex22 <energex22@gmail.com>
2026-09-20 19:04:40 -0500

    Make Vite storefront portable for Pages and custom hosting

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
+  base:'./',
   plugins:[react()],
   server:{port:5173},
   build:{rollupOptions:{input:{main:'index.html',custom:'custom-work.html',product:'product.html',checkout:'checkout.html',shop:'shop.html',orders:'orders.html',order:'order.html',account:'account.html',about:'about.html',faq:'faq.html'}}}
